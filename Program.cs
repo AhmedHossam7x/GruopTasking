588aa4cea21c9845db9c266abdbf6018c68bff0e
AhmedHossam7x <102967579+AhmedHossam7x@users.noreply.github.com>
2025-01-20 16:08:06 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            //=======(>>>>Q1<<<<) Write a program that receives two numbers and prints the larger one, but a multiple.
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Max(x, y) * 2);
+            //=======(>>>>Q2<<<<) Write a program that receives two numbers and prints all the numbers between them.
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            for (int i = Math.Min(x1, y1); i <= Math.Max(x1, y1); i++)
+                Console.Write(i + " ");
         }
     }
 }
