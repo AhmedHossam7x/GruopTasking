270b0222b5a8e8d09f8785739d86a1990ff25277
AhmedHossam7x <102967579+AhmedHossam7x@users.noreply.github.com>
2025-01-20 16:29:24 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            //=======(>>>>Q5<<<<) Write a program that receives a 4-digit number and prints if the ones digit and the last digit are equal.
+            int x = int.Parse(Console.ReadLine());
+            if(x > 100)
+            {
+                int r = x % 10;
+                while (x > 9)
+                    x /= 10;
+                if (r == x)
+                    Console.WriteLine("Is equal");
+                else
+                    Console.WriteLine("Is not equal");
+            } else
+                Console.WriteLine("Invaild number");
         }
     }
 }
