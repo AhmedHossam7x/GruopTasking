b1f3ab659d16a5035b593a2892c1244869fbda3f
AhmedHossam7x <102967579+AhmedHossam7x@users.noreply.github.com>
2025-01-20 16:22:25 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            //=======(>>>>Q4<<<<) Write a program that receives a three-digit number and prints if the number in the last digit is even or odd.
+            int x = int.Parse(Console.ReadLine());
+            if(x < 1000 && x >= 100)
+            {
+                var r = x / 100;
+                if (r % 2 == 0)
+                    Console.WriteLine("even");
+                else
+                    Console.WriteLine("odd");
+            }
+            else
+                Console.WriteLine("Invaild number");
         }
     }
 }
